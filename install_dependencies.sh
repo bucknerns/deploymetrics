e105eb7134e999d5de208501efd48e666e5a7952
Nathan Buckner <bucknerns@gmail.com>
2016-06-07 01:48:43 -0500

    fixed config problems

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,10 +5,10 @@ dpkg -i elasticsearch-2.3.1.deb
 echo 'server {
 	listen 80 default_server;
 	server_name _;
-        include /etc/nginx/uwsgi-enabled/*.conf;
+        include /configs/*.conf;
 }'> /etc/nginx/sites-enabled/default
 
-echo network.bind_host: 127.0.0.1 > /etc/elasticsearch/elasticsearch.yml
+echo network.host: 127.0.0.1 > /etc/elasticsearch/elasticsearch.yml
 echo http.port: 9200  >> /etc/elasticsearch/elasticsearch.yml
 service nginx restart
 service elasticsearch restart
