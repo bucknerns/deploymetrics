5b7927668c1ef4d8c7e7fa5da1eaea648e0520d4
bucknerns <nathan.buckner@rackspace.com>
2016-07-01 09:33:09 -0500

    Update install_elasticsearch.sh

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -9,7 +9,7 @@ wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distrib
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -qy oracle-java8-installer
 dpkg -i elasticsearch-2.3.1.deb
 echo network.host: 127.0.0.1 > /etc/elasticsearch/elasticsearch.yml
 echo http.port: 9200  >> /etc/elasticsearch/elasticsearch.yml
